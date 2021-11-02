57d8a09bc2c85223d5f6fc2f3ebe1a3af1d17133
anticher <anticherq@gmail.com>
2021-11-02 10:13:54 +0300

    feat: complete task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+
+process.stdout.write('hello, enter your ToDo\n')
+process.stdin.on('data', (data) => {
+    const text = data.toString()
+    if (text === 'exit\n') {
+        process.exit()
+    }
+    fs.appendFile(path.join(__dirname, 'text.txt'), text, (error, data) => {
+        if(error) return console.error(error.message)
+        console.log('ToDo created, enter your ToDo')
+      })
+})
+
+process.on('SIGINT', function() {
+    process.stdout.clearLine()
+    process.exit()
+})
+
+process.on('exit', () => {
+    process.stdout.write('Bye, bye\n')
+})
